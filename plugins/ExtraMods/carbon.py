23c16bcd6224f29f75bc851547e963c4e5007d91
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2023-12-31 10:47:45 +0530

    Update carbon.py

--- plugins/ExtraMods/carbon.py
+++ plugins/ExtraMods/carbon.py
@@ -30,8 +30,8 @@ async def carbon_func(b, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**ᴍᴀᴅᴇ ʙʏ: @mkn_bots_updates**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/mkn_bots_updates")]]),                   
+        caption="**ᴍᴀᴅᴇ ʙʏ: @QUEENx_GOD**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ꜱᴜᴩᴩᴏʀᴛ ᴜꜱ", url="https://t.me/+fztuVtP1frMwYTk1")]]),                   
     )
     await m.delete()
     carbon.close()
